56b295bfa9ba1b19f0722c5eb16d157f2698d7b8
jctibaquiram <juan.tibaquira24@gmail.com>
2025-05-28 12:59:41 -0500

    App act

--- app.py
+++ app.py
@@ -4,27 +4,20 @@ import plotly.express as px
 import matplotlib.pyplot as plt
 car_data = pd.read_csv("vehicles_us.csv")
 
+st.header("Venta de Vehiculos")
+
 st.subheader("¿Cómo es la distribución de los datos respecto al valor de odómetro?")
 
 hist_button = st.button('Construir histograma')
 if hist_button:
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Histograma: cuenta de odometro')
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
 
-st.title("Análisis de autos")
-
-build_histogram = st.checkbox('Construir un histograma (odómetro)')
+st.subheader("Análisis del precio de autos con respecto al odómetro")
 
 build_scatter = st.checkbox('Construir un diagrama de dispersión (odómetro vs precio)')
-
-if build_histogram:
-    st.write('Histograma del odómetro:')
-    fig, px = plt.subplots()
-    px.hist(car_data["odometer"], bins=10, color='skyblue', edgecolor='black')                  
-    px.set_xlabel('Odómetro')
-    px.set_ylabel('Frecuencia')
-    st.pyplot(fig)
 
 if build_scatter:
     st.write('Diagrama de dispersión: odómetro vs precio')
